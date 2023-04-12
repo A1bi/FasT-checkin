309f166e9a5dc7c070c1689c90ca941fca918e99
Albrecht Oster <albrecht@oster.online>
2023-04-12 18:21:30 +0200

    Add audio feedback

--- FasT-checkin/FasTAudioFeedbackManager.m
+++ FasT-checkin/FasTAudioFeedbackManager.m
@@ -8,18 +8,20 @@
 
 #import "FasTAudioFeedbackManager.h"
 
-#import <AudioToolbox/AudioToolbox.h>
+#import <AVFAudio/AVFAudio.h>
+#import <AudioToolbox/AudioServices.h>
 
-void AudioServicesStopSystemSound(SystemSoundID soundId);
 void AudioServicesPlaySystemSoundWithVibration(SystemSoundID soundId, id arg, NSDictionary *vibrationPattern);
 
 static NSDictionary *successVibrationPattern;
 static NSDictionary *warningVibrationPattern;
 static NSDictionary *errorVibrationPattern;
+static AVAudioPlayer *successSound, *warningSound, *failureSound;
 
 @interface FasTAudioFeedbackManager ()
 
 + (void)vibrateWithPattern:(NSDictionary *)pattern;
++ (AVAudioPlayer *)preparePlayerForSound:(NSString *)soundName;
 
 @end
 
@@ -29,21 +31,35 @@ static NSDictionary *errorVibrationPattern;
     successVibrationPattern = @{ @"Intensity": @0.5, @"VibePattern": @[ @YES, @100 ] };
     warningVibrationPattern = @{ @"Intensity": @0.5, @"VibePattern": @[ @YES, @50, @NO, @50, @YES, @50, @NO, @50, @YES, @50 ] };
     errorVibrationPattern = @{ @"Intensity": @1.0, @"VibePattern": @[ @YES, @500 ] };
+    
+    successSound = [self preparePlayerForSound:@"success"];
+    warningSound = [self preparePlayerForSound:@"warning"];
+    failureSound = [self preparePlayerForSound:@"failure"];
 }
 
 + (void)vibrateWithPattern:(NSDictionary *)pattern {
     AudioServicesPlaySystemSoundWithVibration(kSystemSoundID_Vibrate, nil, pattern);
 }
 
++ (AVAudioPlayer *)preparePlayerForSound:(NSString *)soundName {
+    NSURL *soundFile = [[NSBundle mainBundle] URLForResource:soundName withExtension:@"aif"];
+    AVAudioPlayer *player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundFile error:nil];
+    [player prepareToPlay];
+    return player;
+}
+
 + (void)indicateSuccess {
     [self vibrateWithPattern:successVibrationPattern];
+    [successSound play];
 }
 
 + (void)indicateWarning {
     [self vibrateWithPattern:warningVibrationPattern];
+    [warningSound play];
 }
 
 + (void)indicateError {
     [self vibrateWithPattern:errorVibrationPattern];
+    [failureSound play];
 }
 @end
