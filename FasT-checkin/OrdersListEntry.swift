83260442b32d721a95df274f4c75e18d2bcba8f2
Albrecht Oster <albrecht@oster.online>
2025-11-21 14:07:03 +0100

    Add icon for check-in status

--- FasT-checkin/OrdersListEntry.swift
+++ FasT-checkin/OrdersListEntry.swift
@@ -5,10 +5,33 @@ struct OrdersListEntry: View {
     
     var body: some View {
         HStack {
+            Image(systemName: iconName).foregroundStyle(iconStyle)
             Text(order.sortedFullName)
             Spacer()
             Text(order.number)
                 .font(.system(.body, design: .monospaced))
+        }
+    }
+    
+    private var iconName: String {
+        switch order.checkInStatus {
+        case .partial:
+            return "circle.bottomhalf.filled.inverse"
+        case .full:
+            return "checkmark.circle.fill"
+        default:
+            return "circle"
+        }
+    }
+    
+    private var iconStyle: Color {
+        switch order.checkInStatus {
+        case .partial:
+            return .yellow
+        case .full:
+            return .green
+        default:
+            return .red
         }
     }
 }
